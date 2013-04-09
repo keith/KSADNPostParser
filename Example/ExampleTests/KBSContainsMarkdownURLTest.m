5864309f6fbb00c768d0094439b4d6f9af6f8851
Keith Smiley <keithbsmiley@gmail.com>
2013-04-09 14:27:24 -0400

    Tests for invalid markdown

--- Example/ExampleTests/KBSContainsMarkdownURLTest.m
+++ Example/ExampleTests/KBSContainsMarkdownURLTest.m
@@ -8,28 +8,55 @@
 
 #import "KBSCommon.h"
 
-SpecBegin(ContainsMarkdownURL)
-
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wall"
+SpecBegin(ContainsMarkdown)
 
 describe(@"containsMarkdownURL", ^{
     it(@"should return true when there is a markdown url", ^{
         NSString *post = @"This is a string with a [url](https://github.com/)";
-        expect([[KSADNPostParser shared] containsMarkdownURL:post]).to.beTruthy;
+        expect([[KSADNPostParser shared] containsMarkdownURL:post]).to.equal(true);
     });
     
     it(@"should return true when there are multiple markdown urls", ^{
         NSString *post = @"This is a [string](https://github.com/petejkim/expecta/) with many [markdown](http://daringfireball.net/projects/markdown/) URLs";
-        expect([[KSADNPostParser shared] containsMarkdownURL:post]).to.beTruthy;
+        expect([[KSADNPostParser shared] containsMarkdownURL:post]).to.equal(true);
     });
     
     it(@"should return false when there isn't a markdown url", ^{
         NSString *post = @"This is a post";
-        expect([[KSADNPostParser shared] containsMarkdownURL:post]).to.beFalsy;
+        expect([[KSADNPostParser shared] containsMarkdownURL:post]).to.equal(false);
+    });
+    
+    it(@"should catch invalid markdown", ^{
+        NSString *post = @"This is a post with a [url(http://google.com/)";
+        expect([[KSADNPostParser shared] containsMarkdownURL:post]).to.equal(false);
+    });
+    
+    it(@"should not allow either piece of the markdown to be empty", ^{
+        NSString *post = @"[url]()";
+        expect([[KSADNPostParser shared] containsMarkdownURL:post]).to.equal(false);
+        
+        post = @"[]()";
+        expect([[KSADNPostParser shared] containsMarkdownURL:post]).to.equal(false);
+        
+        post = @"[](http://google.com)";
+        expect([[KSADNPostParser shared] containsMarkdownURL:post]).to.equal(false);
+    });
+    
+    it(@"should not allow invalid characters in the anchor text", ^{
+        NSString *post = @"[@keith](http://google.com)";
+        expect([[KSADNPostParser shared] containsMarkdownURL:post]).to.equal(false);
+        
+        post = @"[#keith](http://google.com)";
+        expect([[KSADNPostParser shared] containsMarkdownURL:post]).to.equal(false);
+        
+        post = @"[ke.th](http://google.com)";
+        expect([[KSADNPostParser shared] containsMarkdownURL:post]).to.equal(false);
+    });
+    
+    it(@"should allow strange URLs", ^{
+        NSString *post = @"[wikipedia](http://en.wikipedia.org/wiki/Monad_(functional_programming))";
+        expect([[KSADNPostParser shared] containsMarkdownURL:post]).to.equal(true);
     });
 });
-
-#pragma GCC diagnostic pop
 
 SpecEnd

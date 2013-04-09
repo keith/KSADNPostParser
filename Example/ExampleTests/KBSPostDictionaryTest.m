5635cd1079e28cbf3286d765372ab3c2d6306855
Keith Smiley <keithbsmiley@gmail.com>
2013-04-09 11:58:24 -0400

    Add complete test for dictionary metadata

--- Example/ExampleTests/KBSPostDictionaryTest.m
+++ Example/ExampleTests/KBSPostDictionaryTest.m
@@ -17,11 +17,28 @@ describe(@"postDictionaryForText", ^{
         expect(dictionary.count).to.equal(0);
     });
     
-    it(@"should return an dictionary with metadata", ^{
+    it(@"should return an dictionary with correct metadata", ^{
         NSString *post = @"This is a string with a [url](https://github.com/)";
+        NSString *cleanPost = @"This is a string with a url";
         NSDictionary *dictionary = [[KSADNPostParser shared] postDictionaryForText:post];
-        expect(dictionary).notTo.beNil;
-        expect(dictionary.count).to.beGreaterThan(1);
+        expect(dictionary.count).to.equal(2);
+        
+        NSString *postText = [dictionary valueForKey:TEXT_KEY];
+        expect(postText).to.equal(cleanPost);
+        
+        NSDictionary *entities = [dictionary valueForKey:ENTITIES_KEY];
+        expect(entities.count).to.equal(1);
+        
+        NSArray *links = [entities valueForKey:LINKS_KEY];
+        expect(links.count).to.equal(1);
+        
+        NSDictionary *link = [links objectAtIndex:0];
+        NSUInteger position = [[link valueForKey:POSITION_KEY] unsignedIntegerValue];
+        expect(position).to.equal(24);
+        NSUInteger length = [[link valueForKey:LENGTH_KEY] unsignedIntegerValue];
+        expect(length).to.equal(3);
+        NSString *url = [link valueForKey:URL_KEY];
+        expect(url).to.equal(@"https://github.com/");
     });
 });
 

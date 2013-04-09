5d3f85d33f7bb5b93d61babbdf6836f328e3deb4
Keith Smiley <keithbsmiley@gmail.com>
2013-04-09 11:32:24 -0400

    Add local constants file for dictionary keys

---
+++ Example/Example/KSConstants.h
@@ -0,0 +1,20 @@
+//
+//  KSConstants.h
+//  Sail
+//
+//  Created by Keith Smiley on 11/6/12.
+//  Copyright (c) 2012 Keith Smiley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// Keys
+FOUNDATION_EXPORT NSString * const TEXT_KEY;
+FOUNDATION_EXPORT NSString * const POSITION_KEY;
+FOUNDATION_EXPORT NSString * const LENGTH_KEY;
+FOUNDATION_EXPORT NSString * const URL_KEY;
+FOUNDATION_EXPORT NSString * const ENTITIES_KEY;
+FOUNDATION_EXPORT NSString * const LINKS_KEY;
+
+@interface KSConstants : NSObject
+@end
---
+++ Example/Example/KSConstants.m
@@ -0,0 +1,20 @@
+//
+//  KSConstants.m
+//  Sail
+//
+//  Created by Keith Smiley on 11/6/12.
+//  Copyright (c) 2012 Keith Smiley. All rights reserved.
+//
+
+#import "KSConstants.h"
+
+// Keys
+NSString * const TEXT_KEY = @"text";
+NSString * const POSITION_KEY = @"pos";
+NSString * const LENGTH_KEY = @"len";
+NSString * const URL_KEY = @"url";
+NSString * const ENTITIES_KEY = @"entities";
+NSString * const LINKS_KEY = @"links";
+
+@implementation KSConstants
+@end

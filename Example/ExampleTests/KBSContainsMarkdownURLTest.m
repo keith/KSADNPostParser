c58e5dbfc633810a514c7711caea2e7b2c3e2924
Keith Smiley <keithbsmiley@gmail.com>
2013-04-09 11:58:12 -0400

    Add test for containing markdown URL

---
+++ Example/ExampleTests/KBSContainsMarkdownURLTest.m
@@ -0,0 +1,30 @@
+//
+//  KBSContainsMarkdownURLTest.m
+//  Example
+//
+//  Created by Keith Smiley on 4/9/13.
+//  Copyright (c) 2013 Keith Smiley. All rights reserved.
+//
+
+#import "KBSCommon.h"
+
+SpecBegin(ContainsMarkdownURL)
+
+describe(@"containsMarkdownURL", ^{
+    it(@"should return true when there is a markdown url", ^{
+        NSString *post = @"This is a string with a [url](https://github.com/)";
+        expect([[KSADNPostParser shared] containsMarkdownURL:post]).to.beTruthy;
+    });
+    
+    it(@"should return true when there are multiple markdown urls", ^{
+        NSString *post = @"This is a [string](https://github.com/petejkim/expecta/) with many [markdown](http://daringfireball.net/projects/markdown/) URLs";
+        expect([[KSADNPostParser shared] containsMarkdownURL:post]).to.beTruthy;
+    });
+    
+    it(@"should return false when there isn't a markdown url", ^{
+        NSString *post = @"This is a post";
+        expect([[KSADNPostParser shared] containsMarkdownURL:post]).to.beFalsy;
+    });
+});
+
+SpecEnd

3349d9d9c0b6a15b5d5a52021ca95621410f9fa3
Keith Smiley <keithbsmiley@gmail.com>
2013-04-11 15:22:59 -0400

    Cleanup

--- Example/Example/KBSAppDelegate.m
+++ Example/Example/KBSAppDelegate.m
@@ -13,17 +13,10 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    NSString *post = @"[wikipedia](http://en.wikipedia.org/wiki/Monad(functional_programming)";
-    if ([[KSADNPostParser shared] containsMarkdownURL:post]) {
-        NSLog(@"a");
-    } else {
-        NSLog(@"B");
-    }
-    
-    post = @"[wikipedia](sometext) abc [stuff](invalid)";
-    [[KSADNPostParser shared] postDictionaryForText:post withBlock:^(NSDictionary *dictionary, NSError *error) {
-        [[NSAlert alertWithError:error] runModal];
-    }];
+  NSString *post = @"[wikipedia](sometext) abc [stuff](invalid)";
+  [[KSADNPostParser shared] postDictionaryForText:post withBlock:^(NSDictionary *dictionary, NSError *error) {
+    [[NSAlert alertWithError:error] runModal];
+  }];
 }
 
 @end

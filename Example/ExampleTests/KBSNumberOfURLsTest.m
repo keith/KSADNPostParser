dfb76a209f338954a6c320e167687aa7397d5433
Keith Smiley <keithbsmiley@gmail.com>
2013-04-11 14:27:05 -0400

    Test for number of URLs

---
+++ Example/ExampleTests/KBSNumberOfURLsTest.m
@@ -0,0 +1,13 @@
+//
+//  KBSNumberOfURLsTest.m
+//  Example
+//
+//  Created by Keith Smiley on 4/11/13.
+//  Copyright (c) 2013 Keith Smiley. All rights reserved.
+//
+
+#import "KBSCommon.h"
+
+SpecBegin(NumberOfURLs)
+    
+SpecEnd
